23535e854aaaafcbf148f46ca903134429a32c23
Muideen27 <ilorimuideen0000@gmail.com>
2024-11-02 15:19:05 +0100

    creating the theme template in jsx

---
+++ churn-app/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createTheme, ThemeProvider, CssBaseline } from '@mui/material';
+
+// Define the primary color
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#e91e63',
+    },
+  },
+});
