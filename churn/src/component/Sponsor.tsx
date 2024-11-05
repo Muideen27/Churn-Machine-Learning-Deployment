a78dff42cbb1bc3802059bdb1a69362b7a473e93
Muideen27 <ilorimuideen0000@gmail.com>
2024-11-05 09:50:59 +0100

    Churn sponsors

--- churn/src/component/Sponsor.tsx
+++ churn/src/component/Sponsor.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 
 const Sponsor: React.FC = () => {
   return (
-    <Box className="tw-flex tw-flex-col tw-items-center tw-mt-10">
-      <Typography variant="subtitle1" className="tw-font-semibold tw-mb-4 tw-text-gray-700">
-        Sponsored By
-      </Typography>
-      <Box className="tw-flex tw-justify-center tw-space-x-8">
-        <img src="src/assets/01.png" alt="Sponsor 1" className="tw-h-12 tw-w-auto tw-max-w-full"
-            style={{ maxWidth: '50%' }}/>
-        <img src="/src/assets/03.svg" alt="Sponsor 2" className="tw-h-12 tw-w-auto" />
-        <img src="/src/assets/04.svg" alt="Sponsor 3" className="tw-h-12 tw-w-auto" />
+    <Box sx={{ mt: 10, textAlign: 'center' }}>
+      <Box sx={{ display: 'flex', justifyContent: 'center', gap: 4, alignItems: 'center', mt: 2 }}>
+        <img src="/src/assets/01.png" alt="Sponsor 1" style={{ maxWidth: '10%', height: 'auto' }} />
+        <img src="/src/assets/03.svg" alt="Sponsor 2" style={{ maxWidth: '10%', height: 'auto' }} />
+        <img src="/src/assets/04.svg" alt="Sponsor 3" style={{ maxWidth: '10%', height: 'auto' }} />
       </Box>
     </Box>
   );
