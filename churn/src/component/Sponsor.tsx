86b0f311a8145a56c11a9140f0658b30865a1704
Muideen27 <ilorimuideen0000@gmail.com>
2024-11-06 22:09:09 +0100

    Stambic logo adjusted to normal

--- churn/src/component/Sponsor.tsx
+++ churn/src/component/Sponsor.tsx
@@ -6,7 +6,7 @@ const Sponsor: React.FC = () => {
     <Box sx={{ mt: 10, textAlign: 'center' }}>
       <Box sx={{ display: 'flex', justifyContent: 'center', gap: 4, alignItems: 'center', mt: 2 }}>
         <img src="/src/assets/01.png" alt="Sponsor 1" style={{ maxWidth: '10%', height: 'auto' }} />
-        <img src="/src/assets/03.svg" alt="Sponsor 2" style={{ maxWidth: '10%', height: 'auto' }} />
+        <img src="/src/assets/03.svg" alt="Sponsor 2" style={{ maxWidth: '20%', height: 'auto' }} />
         <img src="/src/assets/04.svg" alt="Sponsor 3" style={{ maxWidth: '10%', height: 'auto' }} />
       </Box>
     </Box>
