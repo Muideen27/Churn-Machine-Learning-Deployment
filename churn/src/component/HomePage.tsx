436890395e65884323c6509ccdd029959315a31a
Muideen27 <ilorimuideen0000@gmail.com>
2024-11-09 07:38:11 +0100

    page adjustment

--- churn/src/component/HomePage.tsx
+++ churn/src/component/HomePage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Typography, Container, Divider, Grid, Button } from '@mui/material';
+import { Box, Typography, Container, Divider, Button } from '@mui/material';
 import MainHeader from './MainHeader';
 
 const HomePage: React.FC = () => {
