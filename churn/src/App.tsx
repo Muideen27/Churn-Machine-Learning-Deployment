7a2a8d586a8649a7d87a8cde68c0cfcfb4b4397d
Muideen27 <ilorimuideen0000@gmail.com>
2024-11-18 15:34:13 +0100

    Prediction page routing

--- churn/src/App.tsx
+++ churn/src/App.tsx
@@ -5,6 +5,7 @@ import LandingPage from './component/LandingPage';
 import HomePage from './component/HomePage';
 import ModelAnalysis from './component/ModelAnalysis';
 import EDA from './component/EDA';
+import PredictionPage from './component/PredictionPage';
 
 // Define the theme with Poppins font and primary color
 const theme = createTheme({
@@ -24,9 +25,12 @@ const App: React.FC = () => {
       <CssBaseline />
       <Router>
         <Routes>
+          {/* Route for LandingPage */}
           <Route path="/" element={<LandingPage />} />
           {/* Route for HomePage */}
           <Route path="/home" element={<HomePage />} />
+          {/* Route for Prediction Page */}
+          <Route path="/predict" element={<PredictionPage />} />
           {/* Route for Machine Learning Model Analysis Page */}
           <Route path='ml-analysis'element={<ModelAnalysis />} />
           {/* Route for EDA Page */}
---
+++ churn/src/component/PredictionPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PredictionPage: React.FC = () => {
+  return (
+    <div>
+      <h1>PredictionPage</h1>
+    </div>
+  )
+}
+
+export default PredictionPage
