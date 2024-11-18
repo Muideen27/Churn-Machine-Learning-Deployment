e54e3f2b06f05054236ac0dc6f21edfd6ab6c239
Muideen27 <ilorimuideen0000@gmail.com>
2024-11-18 12:41:52 +0100

    Starting with database visualization

---
+++ churn-api/auth/routes.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, request, jsonify
+from ..models import User
+from .. import db, bcrypt
+from flask_jwt_extended import create_access_token
+
+auth_bp = Blueprint('auth', __name__)
+
+@auth_bp.route('/signup', methods=['POST'])
+def signup():
+    data = request.json
+    hashed_pw = bcrypt.generate_password_hash(data['password']).decode('utf-8')
+    user = User(email=data['email'], password=hashed_pw)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify(message="User registered successfully!"), 201
+
+@auth_bp.route('/signin', methods=['POST'])
+def signin():
+    data = request.json
+    user = User.query.filter_by(email=data['email']).first()
+    if user and bcrypt.check_password_hash(user.password, data['password']):
+        access_token = create_access_token(identity=user.email)
+        return jsonify(access_token=access_token), 200
+    return jsonify(message="Invalid credentials!"), 401
