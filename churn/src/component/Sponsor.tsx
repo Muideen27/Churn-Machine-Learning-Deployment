bdfc3b776da93a26e9af861c2862c21fb1e8d0c5
Muideen27 <ilorimuideen0000@gmail.com>
2024-11-29 06:28:56 +0100

    path update

--- churn/src/component/Sponsor.tsx
+++ churn/src/component/Sponsor.tsx
@@ -5,9 +5,9 @@ const Sponsor: React.FC = () => {
   return (
     <Box sx={{ mt: 10, textAlign: 'center' }}>
       <Box sx={{ display: 'flex', justifyContent: 'center', gap: 4, alignItems: 'center', mt: 2 }}>
-        <img src="/src/assets/01.png" alt="Sponsor 1" style={{ maxWidth: '10%', height: 'auto' }} />
-        <img src="/src/assets/03.svg" alt="Sponsor 2" style={{ maxWidth: '20%', height: 'auto' }} />
-        <img src="/src/assets/04.svg" alt="Sponsor 3" style={{ maxWidth: '10%', height: 'auto' }} />
+        <img src="/01.png" alt="Sponsor 1" style={{ maxWidth: '10%', height: 'auto' }} />
+        <img src="/03.svg" alt="Sponsor 2" style={{ maxWidth: '20%', height: 'auto' }} />
+        <img src="/04.svg" alt="Sponsor 3" style={{ maxWidth: '10%', height: 'auto' }} />
       </Box>
     </Box>
   );
